4ad513201802c6f410425086fae0cc68e519ab2f
TotalEnnui <aprpkp@gmail.com>
2019-07-13 10:50:53 -0400

    Complete circle

--- circle.js
+++ circle.js
@@ -22,8 +22,8 @@ function Func(){
     var val = 100;
     const MAX = 50;
     const POINTSIZE = 1;
-    CONST XPOS = 100;
-    CONST YPOS = 100;
+    const XPOS = 100;
+    const YPOS = 100;
 
     var x = 0;
     var y = 0;
@@ -31,19 +31,20 @@ function Func(){
     var yPos = 0;
     var radius = MAX;
 
-    for(i = 1; i < MAX; i++){
+    for(i = 1; i < MAX+150; i++){
         val = i/100;
         val = Math.PI * val;
         x = Math.sin(val);
         y = Math.cos(val);
         x = Math.round(x * radius);
         y = Math.round(y * radius);
-        console.log("i = ", i, "x = ", x, "y = ", y);
+        console.log("x = ", x, "y = ", y);
         
         ctx.fillStyle = "#ff2626";
         ctx.beginPath();
         xPos = XPOS + x;
-        yPos - YPOS + y;
+        yPos = YPOS + y;
+        console.log(xPos, yPos);
         ctx.arc(xPos, yPos, POINTSIZE, 0, Math.PI * 2, true);
         ctx.fill();
     }
